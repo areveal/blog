8503cabbf5b52942aa3d7b6415274485b4763a12
Cole Reveal <alexi.reveal@gmail.com>
2014-06-26 14:00:39 -0500

    added rolldice file to test getting variables in laravel

--- app/routes.php
+++ app/routes.php
@@ -21,3 +21,28 @@ Route::get('/resume', function(){
 Route::get('/portfolio', function(){
 	return "This will eventually be my portfolio";
 });
+
+Route::get('/sayhello/{name}', function($name)
+{
+    if (strtolower($name) == "cole")
+    {
+        return Redirect::to('/');
+    }
+    else
+    {
+    	$data = ['name' => ucfirst($name)];
+        return View::make('temp.my-first-view')->with($data);
+    }
+});
+
+Route::get('/rolldice', function(){
+	$roll = mt_rand(1,6);
+	return View::make('temp.rolldice')->with('roll',$roll);
+});
+
+Route::get('/rolldice/{guess}', function($guess){
+	$roll = mt_rand(1,6);
+	$data = ['roll'=>$roll,
+			 'guess'=>$guess];
+	return View::make('temp.rolldice')->with($data);
+});
