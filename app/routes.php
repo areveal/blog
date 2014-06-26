7cea27309a7b2c36646c05e7a0722975a6b61650
Cole Reveal <alexi.reveal@gmail.com>
2014-06-26 11:22:00 -0500

    added routes on blog.dev for resume and portfolio

--- app/routes.php
+++ app/routes.php
@@ -10,8 +10,27 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-
-Route::get('/', function()
-{
+Route::get('/', function(){
 	return View::make('hello');
+});
+
+
+Route::get('/sayhello/{name}', function($name)
+{
+    if (strtolower($name) == "cole")
+    {
+        return Redirect::to('http://www.espn.com');
+    }
+    else
+    {
+        return "Hello, $name!";
+    }
+});
+
+Route::get('/resume', function(){
+	return "This will eventually be my resume";
+});
+
+Route::get('/portfolio', function(){
+	return "This will eventually be my portfolio";
 });
